073e150eb0beaab3a91ac9291a626d37317219de
WMHillock <bugorskiyvv@gmail.com>
2022-06-07 16:37:35 +0300

    Deep alpha, server class in work

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,5 +1,41 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    /*TODO класс сервер, хз есть ли смысл еще и БД городить отдельно можно и в сервере хранить инфу
-        у нас все равно более чем 2000 экземпляров Abiturient не будет, с другой стороны, на перспективу ...
-    */
-}
+    public static void main(String[] args) {
+        /*TODO класс сервер, хз есть ли смысл еще и БД городить отдельно можно и в сервере хранить инфу
+            у нас все равно более чем 2000 экземпляров Abiturient не будет, с другой стороны, на перспективу ...
+            class in work
+        */
+
+        try (ServerSocket serverSocket = new ServerSocket(8989)) {
+            System.out.println("Запустили наш чудный сервер!");
+            while (true)
+            try (Socket socket = serverSocket.accept();
+                 BufferedWriter writer =
+                         new BufferedWriter(
+                                 new OutputStreamWriter(
+                                         socket.getOutputStream()
+                                 )
+                         );
+                 BufferedReader reader =
+                         new BufferedReader(
+                                 new InputStreamReader(
+                                         socket.getInputStream()
+
+                                 ))) {
+                String request = reader.readLine();
+                System.out.println(request);
+                String response = request.length() > 0 ? "Проверяем сервер: " + request.length() : "";
+                System.out.println(response);
+                writer.write("Ты на территории свервера!");
+                writer.newLine();
+                writer.flush();
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    }
